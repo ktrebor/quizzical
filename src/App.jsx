acefa56550d5ea94ff937305b1b9563de4e92e9b
Robert <robert.kedves.90@gmail.com>
2023-01-04 18:57:25 +0200

    simplified the code

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,11 @@ function App() {
   const [quiz, setQuiz] = React.useState(false)
   const [quizData, setQuizData] = React.useState([])
 
-  function shuffleTransformAnswers(answers) {
-    answers.sort((a, b) => 0.5 - Math.random())   
-    const newAnswerArray = []
-    answers.forEach(item => {
-      newAnswerArray.push({
-        id: nanoid(),
-        options: item,
-        isSelected: false
-      })
-    })
-    return newAnswerArray
-  }
-
   React.useEffect(() => {
     async function getQuizData() {
       const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-      const data = await res.json()   
+      const data = await res.json()
+
       let myQuizData = []
       data.results.forEach(item => {
         myQuizData.push({
@@ -39,37 +27,44 @@ function App() {
     getQuizData()
   }, [])
 
+  function shuffleTransformAnswers(answers) {
+    answers.sort((a, b) => 0.5 - Math.random())   
+    const newAnswerArray = []
+    answers.forEach(item => {
+      newAnswerArray.push({
+        answerId: nanoid(),
+        options: item,
+        isSelected: false
+      })
+    })
+    return newAnswerArray
+  }
+
   const quizElements = quizData.map(item => {
-    const questionId = item.answers.map(item => item.id)
-    
+
     return ( 
       <Quiz 
         key={item.id}
-        id={item.id}
-        question={item.question}
-        answers={item.answers}
-        isSelected={item.answers.map(element => {element.isSelected})}
-        correctAnswer={item.correctAnswer}
-        holdAnswer={() => holdAnswer(item.id, questionId)}
+        {...item}
+        handleClick={holdAnswer}
       />
     )
   })
 
-  function holdAnswer(id, questionId) {
-    const found = questionId.find(element => element === element.id)
-    
-    setQuizData(prevQuizData => prevQuizData.map(item => {
-      
-      return item.id === id ?
-        {...item,
-          answers: item.answers.map((element) => 
-            element.id === found ?
-            {...element, isSelected: !element.isSelected} :
-            element
-          )
-        } :
-        {...item}
-    }))
+  
+
+  function holdAnswer(answerId) {
+  
+    quizData.map(item => {
+      item.answers.map(element => {
+        if (answerId === element.answerId) {
+          console.log("this far")
+        } else {
+          console.log("noo")
+        }  
+      })
+    })
+
   }
 
   function startQuiz() {
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,27 +1,27 @@
 import React from "react"
 
-function Quiz (props) {
+function Quiz (props) {    
     const allAnswers = props.answers.map(item => {
+
         const styles = {
             backgroundColor: item.isSelected ? "#D6DBF5" : "white"
         }
 
         return (
             <button 
-                key={item.id}
-                id={item.id}
+                key={item.answerId}
+                id={item.answerId}
                 style={styles}
-                onClick={() => props.holdAnswer(props.questionId)}
-                value={item.options}
+                onClick={() => props.handleClick(item.answerId)}
             >
                 {item.options}
             </button>
         )
     })
-
+    
     return (
         <div className="question-container">
-            <h3 className="question-title">{props.question}</h3>
+            <h3 id={props.id} className="question-title">{props.question}</h3>
             <div className="question-answers">
                 {allAnswers}
             </div>
